97a19395eccca1f969b361a78014600f870062f3
rcipdev <ruchikpravasi@gmail.com>
2025-04-20 02:19:51 -0700

    chore: env var update

--- api_routes/nl_chat.py
+++ api_routes/nl_chat.py
@@ -6,9 +6,9 @@ import os
 
 router = APIRouter()
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
-DB_NAME = "hr"
-COLLECTION_NAME = "employees"
+MONGO_URI = os.getenv("MONGO_URI")
+DB_NAME = os.getenv("MONGO_DB_NAME")
+COLLECTION_NAME = os.getenv("MONGO_COLLECTION_NAME")
 
 client = MongoClient(MONGO_URI)
 collection = client[DB_NAME][COLLECTION_NAME]
